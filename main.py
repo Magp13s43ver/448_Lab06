40c4a488ac76d23d7598d5f3879f1cc03eaffdd1
Brandon Teh <yteh@1005-11.eecs.ku.edu>
2015-03-27 12:44:29 -0500

    Updates

--- main.py
+++ main.py
@@ -4,30 +4,72 @@
 #Lab06
 #Date Due: 4/3/2015
 
+import sys
+
 #Read file
 file1 = file('A.csv', 'r')
 file2 = file('B.csv', 'r')
+file3 = file('C.csv', 'r')
+#Store files in an array
+filemat = [file1,file2,file3];
 
-#Create two arrays to store the matrix
+#Create three arrays to store the matrices
 matrix1 = []
 matrix2 = []
+matrix3 = []
+#Store matrices in an array
+matrix = [matrix1, matrix2, matrix3];
 
+#Splitting the values in the file and store them in matrices
+for i in range(0,len(filemat)):
 #For each line in file1
-for line in file1:
-	temp = line.split(",")
+	for line in filemat[i]:
+		temp = line.split(",")
+		
+		for j in range(0,len(temp)):
+			temp[j] = float(temp[j])
 	
-	for i in range(0,len(temp)):
-		temp[i] = float(temp[i])
-	
-	matrix1.append(temp)
-print(matrix1)
+		matrix[i].append(temp)
 
-#For each line in file2
-for line in file2:
-	temp = line.split(",")
+#Printing the matrices
+for i in range(0,len(matrix)):
+	s = "Matrix " + repr(i+1) + " = " + repr(matrix[i]);
+	print(s)
+
+#Check if the matrices are valid
+for i in range(0,len(matrix)):
+	flag = 0
+
+	check = len(matrix[i][0])
 	
-	for i in range(0,len(temp)):
-		temp[i] = float(temp[i])
-	
-	matrix2.append(temp)
-print(matrix2)
+	for j in range(1, len(matrix[i])):
+    		check1 = len(matrix[i][j])
+		
+    		if check != check1:
+        		flag = 1
+        		break
+    		else:
+        		flag = 0
+        
+	if flag == 1:
+		s = "Error. Matrix " + repr(i+1) + " is not valid.";
+    		print(s)
+    		sys.exit()
+	else:
+		s = "Matrix " + repr(i+1) + " is valid.";
+    		print(s)
+    		
+#function assumes the matricies are matricies
+#the funciton will try to multiply them in the order they are passed in
+#otherwise it will decare them to have invalid dimensions
+def multiply(a, b):
+        if (len (a[0]) == len (b)):
+                tempMtx = [[0 for i in range(len(b[0]))]for j in range(len(a))]
+                for i in range(len(a)):
+                        for j in range (len (b[0])):
+                                for k in range (len(a[0])):
+                                        tempMtx[i][j] = tempMtx[i][j] + (a[i][k]*b[k][j])
+                return tempMtx
+        else:
+                print("Not multiplyable, invalid dimensions")
+
